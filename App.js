775ccf3bad5a0c470133e320d178e405ecf3ffc9
gtheox <gteodoro197@gmail.com>
2025-02-17 11:54:58 -0300

    alteracoes de estilo

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, Text } from "react-native";
+import Estilo from "./Components/Estilo";
 import Primeiro, { Segundo, Terceiro } from "./Components/Primeiro";
+import Somar from "./Components/Somar";
 
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text style={style.txt}>
+    <SafeAreaView style={Estilo.corpo}>
+      <Text style={Estilo.txt}>
         Gabriel Teodoro
       </Text>
       <Primeiro />
       <Segundo />
       <Terceiro />
+      <Somar num1="10" num2="20" />
     </SafeAreaView >
   )
 }
-
-const style = StyleSheet.create({
-  txt: {
-    color: "#0000CD"
-  }
-})
 
 export default App
---
+++ Components/Estilo.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    txt: {
+        color: "#0000CD",
+        fontSize: 60,
+    },
+    corpo: {
+        flex: 1,
+        backgroundColor: "#CD0000",
+        alignItems: "center",
+        justifyContent: "center"
+
+    }
+})
